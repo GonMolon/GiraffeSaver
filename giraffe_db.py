1c4613becfa268ca102e2867f725ab78d49287e6
Ricard Gascons <gascons1995@gmail.com>
2017-01-21 22:08:04 -0500

    Load and Store graphs

--- giraffe_db.py
+++ giraffe_db.py
@@ -1,17 +1,26 @@
 import sys
+import pickle
+import io
+from os import listdir
+from os.path import isfile, join
 
 from graph_generator import generate_graph
 
-
 def store_giraffe(path):
     graph = generate_graph(path)
+    f = io.open(path, 'rb')
+    pickle.dump(graph, f)
+    f.close()
 
 
 def search_giraffe(path):
-    graph = generate_graph(path)
-
+    onlyfiles = [join(path, f) for f in listdir(path) if isfile(join(path, f))]
+    print onlyfiles
+    graph_list = [generate_graph(file) for file in onlyfiles]
+    return graph_list
 
 def main():
+
     store_giraffe(sys.argv[1])
 
 
