8e59fd2832df5625be282dd95dba5f972af250f0
Gonzalo Solera <gonzalo1796@gmail.com>
2017-01-21 22:09:45 -0500

    Adding ratio calculation

--- node.py
+++ node.py
@@ -12,3 +12,19 @@ class Node:
         x1, y1 = self.pos
         x2, y2 = other.pos
         return math.sqrt(math.pow(x1 - x2, 2) + math.pow(y1 - y2, 2))
+
+    def get_ratio(self, other):
+        ratios = [self.area/other.area, self.outline/other.outline]
+        mean_ratio = Node.__mean(ratios)
+        variance_ratio = Node.__variance(mean_ratio, ratios)
+        return mean_ratio, variance_ratio
+
+    @staticmethod
+    def __mean(list):
+        return sum(list)/len(list)
+
+    @staticmethod
+    def __variance(mean, list):
+        aux = [math.pow(x-mean, 2) for x in list]
+        return sum(aux)/len(aux)
+
