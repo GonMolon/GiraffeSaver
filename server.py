1ceb1deaaee0dd6d351841ab7f417edddfb1b521
Pau Oliver <pau.oliver@est.fib.upc.edu>
2017-01-22 04:28:15 -0500

    Add index.html with rectangle area selector of an loaded image

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 
@@ -6,6 +6,11 @@ app = Flask(__name__)
 def ricard_si_ens_has_de_preguntar_preguntes_de_p3_ja_faig_flask():
     return "Hola Pau"
 
+
+@app.route('/submit_image_to_study', methods=['POST'])
+def addRegion():
+    print("I got it!")
+    print(request.form['projectFilepath'])
 
 if __name__ == '__main__':
     app.run()
