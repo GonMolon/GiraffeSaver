a2effe3c339e4e31b19eba5334aa58e674baf8ce
Twinone <twinonedevs@gmail.com>
2017-01-22 08:43:54 +0100

    Update crop to a function

--- spots.py
+++ spots.py
@@ -8,31 +8,35 @@ def process(frame):
 
     return gray
 
-img = cv2.imread(sys.argv[1], 0)
-img = cv2.resize(img, (900,600))
-ret,img = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
+
+def spots(img):
+    img = cv2.resize(img, (900,600))
+    ret,img = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
 
 
-img = cv2.medianBlur(img, 3)
+    img = cv2.medianBlur(img, 3)
 
-kernel = np.ones((4,4),np.uint8)
-img = cv2.erode(img, kernel, iterations = 1)
-img = cv2.medianBlur(img, 7)
-kernel = np.ones((4,4),np.uint8)
-img = cv2.dilate(img, kernel, iterations = 1)
+    kernel = np.ones((4,4),np.uint8)
+    img = cv2.erode(img, kernel, iterations = 1)
+    img = cv2.medianBlur(img, 7)
+    kernel = np.ones((4,4),np.uint8)
+    img = cv2.dilate(img, kernel, iterations = 1)
 
-img = cv2.medianBlur(img, 9)
-
-
-#out = process(img)
-
-cv2.imshow('frame', img)
+    img = cv2.medianBlur(img, 9)
+    return img
 
 
-# Wait for quit key
-while True:
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+def test():
+    img = cv2.imread(sys.argv[1], 0)
+    img = spots(img)
+    cv2.imshow('frame', img)
 
-# When everything done, release the capture
-cv2.destroyAllWindows()
+    # Wait for quit key
+    while True:
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # When everything done, release the capture
+    cv2.destroyAllWindows()
+
+test()
